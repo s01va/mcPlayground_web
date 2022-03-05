8ee4b7b8033d122f0608c047ff8a43e0606db83a
s01va <jinn0525@gmail.com>
2022-03-05 16:41:49 +0900

    spring init

---
+++ src/test/java/com/mcplayground/webboard/WebboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcplayground.webboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
